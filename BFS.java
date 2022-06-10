8f83418bf0e9e7989ba81a4dbd55bd6b4289110d
Sergei Vilensky <sergey3282@gmail.com>
2022-06-10 16:15:10 +0300

    /

--- BFS.java
+++ BFS.java
@@ -1,35 +1,69 @@
 import java.security.SecureRandom;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
+import java.util.concurrent.SynchronousQueue;
 
 public class BFS<E> {
-    private boolean bidirectional;
-    private boolean weighted;
-    private final Graph<E> graph;
-
-    private E startElement;
+    private final boolean bidirectional;
+    private final boolean weighted;
+    private final Map<E, HashMap<E, Integer>> graph;
+    //map prev? +speed, -space
+    private final E startElement;
+    private E endElement;
     private List<List<E>> layers= new ArrayList<>();
 
     public BFS(Graph<E> graph, E startElement){
-        this.graph = graph;
+        if(!graph.getGraph().containsKey(startElement))
+            throw new RuntimeException("no such start element in graph");
+        this.graph = graph.getGraph();
         this.startElement = startElement;
         this.bidirectional = graph.isBidirectional();
         this.weighted = graph.isWeighted();
+        init();
+    }
+
+    public BFS(Graph<E> graph, E startElement, E endElement){
+        this(graph, startElement);
+        if(!graph.getGraph().containsKey(endElement))
+            throw new RuntimeException("no such end element in graph");
+        this.endElement = endElement;
     }
 
     public BFS(Graph<E> graph){
-        this.graph = graph;
+        this.graph = graph.getGraph();
         this.startElement = pickRandomVertex();
         this.bidirectional = graph.isBidirectional();
         this.weighted = graph.isWeighted();
+        init();
     }
 
     //used when root vertex is not passed in constructor
     private E pickRandomVertex(){
         SecureRandom rnd = new SecureRandom();
-        List<E> keys = new ArrayList<>(graph.getVertexes());
+        List<E> keys = new ArrayList<>(graph.keySet());
         E element = keys.get(rnd.nextInt(keys.size()));
         return element;
+    }
+
+    //build BFS from start element
+    public void init(){
+        Queue<E> fifo = new LinkedList<>();
+        E current = startElement;
+        fifo.add(current);
+        Set<E> visited = new HashSet<>();
+        visited.add(current);
+
+        while(!fifo.isEmpty()){
+            current = fifo.poll();
+            List<E> layer = new ArrayList<>();
+            for(E e: graph.get(current).keySet()){
+                if(!visited.contains(e)){
+                    visited.add(e);
+                    layer.add(e);
+                    fifo.add(e);
+                }
+            }
+            if(!layer.isEmpty())
+                layers.add(layer);
+        }
     }
 }
--- MainTest.java
+++ MainTest.java
@@ -1,6 +1,7 @@
 public class MainTest {
     public static void main(String[] args) {
         Graph<Integer> integerGraph = new Graph<>(false, true);//add check for weighted
+        Graph<Integer> integerGraph1 = new Graph<>(false, false);//add check for weighted
 
         integerGraph.addEdge(1,2,15);
         integerGraph.addEdge(3,2,2);
@@ -11,14 +12,26 @@ public class MainTest {
         integerGraph.addEdge(10,9,6);
         integerGraph.addEdge(4,7,7);
         integerGraph.addEdge(3,9,7);
-        integerGraph.removeEdge(4,10);
+
+        integerGraph1.addEdge(1,2,15);
+        integerGraph1.addEdge(3,2,2);
+        integerGraph1.addEdge(4,9,30);
+        integerGraph1.addEdge(1,7,12);
+        integerGraph1.addEdge(6,2,5);
+        integerGraph1.addEdge(6,7,25);
+        integerGraph1.addEdge(10,9,6);
+        integerGraph1.addEdge(4,7,7);
+        integerGraph1.addEdge(3,9,7);
+        integerGraph1.removeEdge(4,10);
 
         //System.out.println(integerGraph);
         //System.out.println(integerGraph.vertexCount());
         //System.out.println(integerGraph.edgeCount());
 
-        Graph<Integer> copy = new Graph<>();
+        /*Graph<Integer> copy = new Graph<>();
         copy.copy(integerGraph);
         System.out.println(copy);
+
+        System.out.println(integerGraph1);*/
     }
 }
